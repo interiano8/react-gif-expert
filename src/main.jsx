1b1b583a6a4486cbb23ed635d559f857342386f9
Interiano <josue.interiano88@gmail.com>
2024-01-12 23:38:31 -0600

    creado docs

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp.jsx'
 import './style.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <GifExpertApp />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GifExpertApp  />
   </React.StrictMode>,
 )
